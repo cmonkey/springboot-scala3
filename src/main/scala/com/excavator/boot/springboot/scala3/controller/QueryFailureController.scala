05c1590f9eb0fb906c7850ac7fe4349f44d04a26
CMonkey <42.codemonkey@gmail.com>
2023-06-07 18:00:12 +0800

    implement queryFailureRecord function to QueryFailureController

--- src/main/scala/com/excavator/boot/springboot/scala3/controller/QueryFailureController.scala
+++ src/main/scala/com/excavator/boot/springboot/scala3/controller/QueryFailureController.scala
@@ -1,5 +1,20 @@
 package com.excavator.boot.springboot.scala3.controller
 
-class QueryFailureController {
+import com.excavator.boot.springboot.scala3.service.QueryFailureService
+import org.springframework.web.bind.annotation.{GetMapping, PathVariable, RequestMapping, RestController}
+import reactor.core.publisher.Flux
+
+import java.time.LocalDate
+
+@RestController
+@RequestMapping(value = Array("/query/failure/v1"))
+class QueryFailureController(val queryFailureService: QueryFailureService) {
+
+  @GetMapping(value = Array("/record/{time}"))
+  def queryFailureRecord(@PathVariable("time") time:String) = {
+    val localDate = LocalDate.parse(time)
+    val r = queryFailureService.queryFailure(localDate)
+    Flux.just(r.toArray():_*)
+  }
 
 }
