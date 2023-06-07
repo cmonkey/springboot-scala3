72a408e858af3ae3ef918a91ac087607f40aec2a
CMonkey <42.codemonkey@gmail.com>
2023-06-07 15:55:15 +0800

    init project

---
+++ src/main/java/com/excavator/boot/scala3demo/Scala3DemoApplication.java
@@ -0,0 +1,13 @@
+package com.excavator.boot.scala3demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scala3DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scala3DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excavator/boot/scala3demo/Scala3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excavator.boot.scala3demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scala3DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
