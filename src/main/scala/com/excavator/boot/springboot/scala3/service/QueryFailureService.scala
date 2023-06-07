6c0da301d4547003803ebc793fefa5845dd05cc5
CMonkey <42.codemonkey@gmail.com>
2023-06-07 18:01:07 +0800

    implement queryFailure to QueryFailureService

--- src/main/scala/com/excavator/boot/springboot/scala3/service/QueryFailureService.scala
+++ src/main/scala/com/excavator/boot/springboot/scala3/service/QueryFailureService.scala
@@ -1,5 +1,21 @@
 package com.excavator.boot.springboot.scala3.service
 
-class QueryFailureService {
+import com.excavator.boot.springboot.scala3.entity.FailureRecord
+import com.excavator.boot.springboot.scala3.repository.QueryFailureRepository
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+import java.time.LocalDate
+import java.util.List as JList
+
+@Service
+class QueryFailureService(val queryFailureRepository: QueryFailureRepository) {
+  val log = LoggerFactory.getLogger(classOf[QueryFailureService])
+
+  def queryFailure(time:LocalDate):JList[FailureRecord] = {
+    val startTime = time.atTime(0, 0, 0, 0)
+    val endTime = time.atTime(23,59,59,59)
+    queryFailureRepository.queryFailure(startTime, endTime)
+  }
 
 }
